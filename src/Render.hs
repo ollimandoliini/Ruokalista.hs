655e2678470ec0cc51e565240ca8b05cd35dfdc3
Olli Kortelahti <okortelahti@gmail.com>
2019-09-01 22:11:10 +0300

    Laituri Menu

--- src/Render.hs
+++ src/Render.hs
@@ -5,9 +5,12 @@ module Render where
 import qualified Data.ByteString.Lazy as S
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.Encoding as E
+import qualified Data.Text.Lazy as L
 import Data.Text.Template
 import Text.RawString.QQ
 import Data.Maybe
+import Requests
+import Data.List
 
 
 htmlTemplate :: T.Text
@@ -20,6 +23,7 @@ htmlTemplate = T.pack
         <body>
             <h1>Lounaspaikat Ruoholahdessa 🍔🍟🍕🌮🥙</h1>
             $name
+            $menu
         </body>
     </html>
     |]
@@ -30,11 +34,19 @@ context :: [(T.Text, T.Text)] -> Context
 context assocs x = maybe err id . lookup x $ assocs
   where err = error $ "Could not find key: " ++ T.unpack x
 
-renderedHtml content = substitute htmlTemplate helloContext
-  where
-    helloContext  = context [("name", T.pack $ show content)]
+-- renderedHtml :: Maybe NormalizedMenu -> L.Text
+-- renderedHtml Nothing = substitute htmlTemplate $ context [("name", maybe "Not found" (T.pack . parseContent) content)]
+-- renderedHtml (Just content) = substitute htmlTemplate $ context [("name", T.pack $ parseContent content)]
+
+
+renderedHtml :: Maybe NormalizedMenu -> L.Text
+renderedHtml content =
+  substitute htmlTemplate $ context [("name", maybe "Not found" (T.pack . restaurant) content)
+                                      ,("menu", maybe "Not Found" menuItems content)]
+
+-- menuItems :: NormalizedMenu -> L.Text
+menuItems menu =
+  T.pack $ intercalate "</BR></BR>" (map (\x -> intercalate "</BR>" [title x, itemPrice x, itemProperties x]) (foods menu))
 
 
 
--- parseContent content =
--- map (\x -> (T.pack $ show $ restaurant $ fromJust x) ) content
