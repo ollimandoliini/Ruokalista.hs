6474686c783d12b197755344b3771f88f89492ea
Olli Kortelahti <okortelahti@gmail.com>
2019-09-07 10:36:39 +0300

    add Procfile for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./dist/build/ruokalista-haskell/ruokaista-haskell-exe
--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,10 @@ import Control.Monad.IO.Class
 import qualified Data.Text.Lazy as L
 import System.Environment
 
+import Data.Time.Clock
+import Data.Time.Calendar
+
+
 tupleFromInputString :: String -> Maybe Requests.Date
 tupleFromInputString input = 
   if length stringComponents /= 3
@@ -21,12 +25,15 @@ tupleFromInputString input =
     replaceDashesWithSpaces = map (\c -> if c == '-' then ' '; else c)
 
 
+getDate = getCurrentTime >>= return . toGregorian . utctDay
+
 main = do
-  -- port <- maybe 3000 read <$> lookupEnv "PORT"
   port <- fmap (maybe 3000 read) (lookupEnv "PORT")
   scotty port $ do
     get "/" $ do
-      html "moro"
+      date <- liftIO $ getDate
+      content <- liftIO $ Requests.getMenus date
+      html $ renderedHtml content
 
     get "/:date" $ do
       dateString <- param "date"
